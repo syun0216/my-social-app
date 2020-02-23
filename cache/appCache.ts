4ba35caf28799d5e281cdf471937804fb16358df
syun <517897442@qq.com>
2020-02-23 17:46:28 +0800

    feat: update cache

--- cache/appCache.ts
+++ cache/appCache.ts
@@ -1,6 +1,31 @@
 import {AsyncStorage} from 'react-native'
 import { isJsonString } from '../utils/commonUtils'
 
+const USER_KEY = 'storage_key_user_data'
+
+const setData = (key: string, data: any): Promise<void> => {
+  if(Object.prototype.toString.call(data) == "[object String]") {
+    return AsyncStorage.setItem(key, data);
+  } else {
+    return AsyncStorage.setItem(key, JSON.stringify(data));
+  }
+}
+
+export default class AppStorage {
+  static getUser = (): Promise<any> => {
+    return AsyncStorage.getItem(USER_KEY)
+  }
+  static setUser = (data): Promise<void> => {
+    return setData(USER_KEY, data)
+  }
+  static removeUser = (): void => {
+    AsyncStorage.removeItem(USER_KEY)
+  }
+  static removeAll = (): void => {
+    AsyncStorage.clear()
+  }
+}
+
 function storageFactory(key: string) {
   this.key = key;
   this.setData = function(data) {
